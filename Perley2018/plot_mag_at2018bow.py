266e8cc12355e75fb747cef214c0f784ca24e1bd
Amir Michaelis <amir.m@aminor-tech.com>
2022-10-03 13:27:34 +0300

    use absolute magnitude and save mag mjd and mag err to npz file.

--- Perley2018/plot_mag_at2018bow.py
+++ Perley2018/plot_mag_at2018bow.py
@@ -14,13 +14,16 @@ with open('AT2018bow_photometry_table.1808.00969.json', 'r') as fid:
 
 mjd = np.array([])
 mag = np.array([])
+err = np.array([])
 
 for i, inst in enumerate(dd['inst']):
     if 'V' in inst.upper():
         mjd = np.append(mjd, dd['mjd'][i])
         mag = np.append(mag, dd['mag'][i])
-
+        err = np.append(mag, dd['err'][i])
+mag = mag - 5.*np.log10(60e6/10)
 pl.plot(mjd,mag, 'rx')
 pl.gca().invert_yaxis()
 pl.show()
 
+np.savez('AT2018bow.npz', mjd=mjd, mag=mag, err=err)
