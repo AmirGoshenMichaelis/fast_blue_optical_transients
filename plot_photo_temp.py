2998ac7081b9c3d1fdc1b80d016f5d1b6508c2a0
Amir Michaelis <amirmi@ariel.ac.il>
2023-01-02 17:15:13 +0200

    change dynamic range

--- plot_photo_temp.py
+++ plot_photo_temp.py
@@ -29,8 +29,7 @@ dir  = f'/home/amirm/code/fast_blue_optical_transients/dd/{exp_dir}/'
 odir = f'/home/amirm/code/fast_blue_optical_transients/photosphere/{exp_dir}/'
 os.chdir(dir)
 cmap = pl.cm.get_cmap('coolwarm', 512)
-# temp_limit = [3.7, 4.6]
-temp_limit = [3., 5.]
+temp_limit = [3., 4.6]
 ###
 def find_dynamic_range():
     logT = []
